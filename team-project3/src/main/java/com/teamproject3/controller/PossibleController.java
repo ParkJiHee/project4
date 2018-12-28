1cbff75fac3ab0ba58b351da6282f333789c6b5f
hhol1 <hhol1@seona>
2018-12-28 17:07:45 +0900

    잠재고객컨트롤러

--- team-project3/src/main/java/com/teamproject3/controller/PossibleController.java
+++ team-project3/src/main/java/com/teamproject3/controller/PossibleController.java
@@ -1,5 +1,7 @@
 package com.teamproject3.controller;
 
+import java.util.ArrayList;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
@@ -7,6 +9,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import com.teamproject3.vo.MemberVo;
 
@@ -16,8 +20,8 @@ import com.teamproject3.vo.MemberVo;
 public class PossibleController {
 	
 	@RequestMapping(value= {"/", "/possiblemember.action"},  method = RequestMethod.POST )
-	public String newregister(//@ModelAttribute("membervo") MemberVo member,
-			HttpSession session, HttpServletRequest req) {
+	public String newregister(@ModelAttribute("membervo") MemberVo member,
+			MultipartHttpServletRequest req) {
 		
 //		member = (MemberVo)session.getAttribute("loginuser");
 //		if (member == null) {
@@ -26,6 +30,34 @@ public class PossibleController {
 //		}
 //		
 //		return "studylist/studyregister";
+		
+	/*	MultipartFile attach = req.getFile("file");
+		System.out.println("파일  " + attach);
+
+		ArrayList<St> attachments = new ArrayList<>();
+		if (attach != null && !attach.isEmpty()) {
+			// 파일저장
+			String savedFileName = Util.makeUniqueFileName(attach.getOriginalFilename());
+			String path = req.getServletContext().getRealPath("/resources/studyupload/" + savedFileName);
+			System.out.printf("저장되냐========\n" + path);
+			try {
+				attach.transferTo(new File(path));
+
+				StudyAttach attachment = new StudyAttach();
+				attachment.setS_savedFileName(savedFileName);
+				attachment.setS_userFileName(attach.getOriginalFilename());
+				
+				attachments.add(attachment);
+				System.out.println("파일저장성공" + attachment);
+			} catch (Exception e) {
+
+			}
+		}
+
+		studyvo.setAttachments(attachments);
+		studyService.writeStudy(studyvo);
+		System.out.println("2======"+studyvo);*/
+		
 		
 		return "financial/possiblemember.action";
 	}
